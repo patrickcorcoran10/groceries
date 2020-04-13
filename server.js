db166611d190d45ec55a3114ab136f6b51dfa3a2
Patty C <patrickcorcoran10@gmail.com>
2020-04-13 18:37:36 -0500

    commit with server.js change

--- server.js
+++ server.js
@@ -11,8 +11,12 @@ const db = require("./models");
 
 require("./routes/api-routes")(app);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, ".client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 let PORT = process.env.PORT || 3001;
 db.sequelize.sync().then(function () {
